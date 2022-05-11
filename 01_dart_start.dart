e7007d40e6e96a09ba7111837e7705c8a5f9b10a
Gold-miner354 <101792617+Gold-miner354@users.noreply.github.com>
2022-05-11 21:30:08 +0800

    debug

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -11,58 +11,36 @@ void variables()
     };
     print('name is &name,year is $year,antennaDiameter is $antennaDiameter,flybyObjects is $flybyObjects,image is $image');
 }
-//胖箭头
-void text3()
-{
-    var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
-    flybyObjects.where((name) => name.contains('turn')).forEach(print);
+
+//流程控制语句https://dart.cn/samples#control-flow-statements
+void flow_control() {
+  print('======流程控制语句======');
+  var year = 1977;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+  for (final object in flybyObjects) {
+    print(object);
+  }
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+  while (year < 2016) {
+    year += 1;
+  }
 }
-//流程控制语句:https://dart.cn/samples#control-flow-statements
-void control()
-{
-    var year = 1977;
-    var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
-    if (year >= 2001) {
-        print('21st century');
-    } else if (year >= 1901) {
-        print('20th century');
-    }
 
-    for (final object in flybyObjects) {
-        print(object);
-    }
-
-    for (int month = 1; month <= 12; month++) {
-         print(month);
-    }
-
-    while (year < 2016) {
-        year += 1;
-    }
-    print('year is $year');
-
-}
-//注释:https://dart.cn/samples#comments
-/// 这是一个文档注释。
-/// 文档注释用于为库、类以及类的成员添加注释。
-/// 像 IDE 和 dartdoc 这样的工具可以专门处理文档注释。
-
-/* 也可以像这样使用单斜杠和星号的注释方式 */
+//函数https://dart.cn/samples#functions
 int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
-void main(List<String> args)
-{  
-    //你好，世界
-    print('Hello,world!');
-    //变量
-    variables();
-    //流程控制语句
-    control();
-    //函数
-    var result = fibonacci(20);
-    print('result is $result');
-    //胖箭头
-    text3();
+
+void main() {
+  variables();
+  var result = fibonacci(20);
+  print('fibonacci result is $result');
 }
