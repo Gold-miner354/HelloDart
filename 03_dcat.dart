284fb0b3fd453e495dd5372d731f14227e355a62
Gold-miner354 <1762534041@qq.com>
2022-07-01 10:34:32 +0000

    END

--- 03_dcat.dart
+++ 03_dcat.dart
@@ -1,7 +1,7 @@
+// 命令行程序 （https://dart.dev/tutorials/server/cmdline）
+
 import 'dart:convert';
 import 'dart:io';
-
-//命令行程序(https://dart.cn/tutorials/server/cmdline#overview-of-the-dcat-app-code)
 import 'package:args/args.dart';
 
 const lineNumber = 'line-number';
@@ -9,8 +9,11 @@ const lineNumber = 'line-number';
 void main(List<String> args) {
   exitCode = 0;
   final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+
   ArgResults argResults = parser.parse(args);
+
   final paths = argResults.rest;
+
   dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
 }
 
@@ -18,12 +21,12 @@ Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
   if (paths.isEmpty) {
     print('type exit to quit.');
     while (true) {
-      stdout.write('>>>');
+      stdout.write(
+          '> '); 
       String? line = stdin.readLineSync();
       print('${line}\n');
 
       if (line?.toLowerCase() == 'exit') {
-        //修改终止条件
         print('bye.');
         break;
       }
@@ -34,10 +37,11 @@ Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
       final lines = utf8.decoder
           .bind(File(path).openRead())
           .transform(const LineSplitter());
+
       try {
         await for (final line in lines) {
           if (showLineNumbers) {
-            stdout.write('${lineNumber++}');
+            stdout.write('${lineNumber++} ');
           }
           stdout.writeln(line);
         }
@@ -50,7 +54,7 @@ Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
 
 Future<void> _handleError(String path) async {
   if (await FileSystemEntity.isDirectory(path)) {
-    stderr.writeln('Error:$path is a dirictory.');
+    stderr.writeln('Error: $path is a directory.');
   } else {
     exitCode = 2;
   }
